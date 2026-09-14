5d0a0fd1882e187e504d4d88d78b12db69fc7dc1
Agony <aaritsuyu@gmail.com>
2026-09-14 19:00:15 -0300

    test(native): isolate worker ticket transport and rejection

--- src/native_output/kms_worker/mod.rs
+++ src/native_output/kms_worker/mod.rs
@@ -49,6 +49,8 @@ mod task4_tests;
 mod tests;
 #[cfg(test)]
 mod timing_tests;
+#[cfg(test)]
+mod worker_pacing_tests;
 pub(crate) use bundle::KmsCursorOwner;
 #[cfg(test)]
 pub(crate) use bundle::KmsPrimaryOwner;
